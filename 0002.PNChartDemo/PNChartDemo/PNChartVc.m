b6d240fcd656f9cc101c56e910470714b7f974c0
iot_iMac <2355466532@qq.com>
2018-04-16 10:31:12 +0800

    提交官方demo

--- 0002.PNChartDemo/PNChartDemo/PNChartVc.m
+++ 0002.PNChartDemo/PNChartDemo/PNChartVc.m
@@ -32,36 +32,33 @@
 - (void)drawLineChart {
     
     //创建折线图表
-    PNLineChart *lineChart = [[PNLineChart alloc] init];
-    lineChart.frame = CGRectMake(0, 100, self.view.bounds.size.width, 300);
+    PNLineChart *lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 300)];
+    //lineChart.frame = CGRectMake(0, 100, self.view.bounds.size.width, 300);
     [self.view addSubview:lineChart];
+    lineChart.backgroundColor = [UIColor whiteColor];
+    lineChart.yGridLinesColor = [UIColor grayColor];
+    lineChart.showCoordinateAxis = YES; //是否显示坐标轴,默认NO
+    //统一设置表格标签
+    lineChart.xLabels = @[@"T1",@"T2",@"T3",@"T4",@"T5",@"T6",@"T7"];
+    lineChart.yLabels = @[@"0",@"100℃",@"200℃",@"300℃",@"400℃"];
+    lineChart.yLabelFormat = @"%.1f℃";
     
-    //统一设置画布宽高
-    lineChart.chartMarginTop = 25;
-    lineChart.chartMarginLeft = 25;
-    lineChart.chartMarginBottom = 25;
-    lineChart.chartMarginRight = 25;
     
-    //lineChart.chartCavanWidth = self.view.bounds.size.width - leftMargin - rightMargin;
-    //lineChart.chartCavanHeight = self.view;
-    
-    lineChart.xLabels = @[@"T1",@"T2",@"T3",@"T4"];
-    lineChart.yLabels = @[@"100",@"200",@"300",@"400",@"500",@"600"];
-    
-    NSArray *dataArr = @[@120.3,@250,@200,@400];
+    //设置Y轴的刻度返回
+    //    lineChart.yFixedValueMin = 0.0;
+    //    lineChart.yFixedValueMax = 500;
+    NSArray *line01_yArr = @[@120.3,@250,@200,@400];
     PNLineChartData *data = [[PNLineChartData alloc] init];
     data.color = PNRed;
-    data.inflexionPointStyle = PNLineChartPointStyleCircle;
+    data.inflexionPointStyle = PNLineChartPointStyleCircle; //inflexionPoint:弯曲、拐点
     data.inflexionPointColor = PNGreen;
-    data.itemCount = lineChart.xLabels.count;
-    data.getData = ^PNLineChartDataItem *(NSUInteger item) {
-        PNLineChartDataItem *dataItem = [PNLineChartDataItem dataItemWithY: [dataArr[item] floatValue]];
-        return dataItem;
+    data.itemCount = line01_yArr.count;
+    data.getData = ^PNLineChartDataItem *(NSUInteger idx) {
+        CGFloat yValue = [line01_yArr[idx] floatValue];
+        return [PNLineChartDataItem dataItemWithY:yValue];
     };
     lineChart.chartData = @[data];
     [lineChart strokeChart];
-    
-    
 }
 
 - (void)drawBarChart {
